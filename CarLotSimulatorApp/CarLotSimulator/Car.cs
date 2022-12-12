f5253d202713e4871d9ac8bb1bece784ca8217ae
nathan stephens <nstephens1234567@gmail.com>
2022-12-12 07:22:14 -0600

    initial commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    internal class Car
+    {
+
+        public Car()
+        {
+
+        }
+
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool IsDriveable = false)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;            
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise() 
+        {
+            Console.WriteLine(EngineNoise);
+        }
+
+        public void MakeHonkNoise() 
+        {
+            Console.WriteLine(HonkNoise);
+        }
+    }
+}
+
+    
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -8,19 +8,58 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            //Create a seperate class file called Car  *
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable  *
+            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()  *
+            //The methods should take one string parameter: the respective noise property  *
 
 
-            //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-            //Call each of the methods for each car
+            //Now that the Car class is created we can instanciate 3 new cars  *
+            //Set the properties for each of the cars  *
+            //Call each of the methods for each car   *
+
+            CarLot newLot = new CarLot();            
+            
+            //Custom Constructor\\
+            Car car1 = new Car(2019,"Dodge", "Ram", "Vrooooooom!", "Honk Honk", true);
+                                   
+            car1.MakeEngineNoise();
+            car1.MakeHonkNoise();
+            newLot.CarList.Add(car1);
+
+            //Dot Notation\\
+            Car car2 = new Car();
+                                    
+            car2.Year = 2017;
+            car2.Make = "Chrysler";
+            car2.Model = "Pacifica";
+            car2.EngineNoise = "Weeeeeeee!";
+            car2.HonkNoise = "Meep Meep!";
+            car2.IsDriveable= true;
+
+            car2.MakeEngineNoise();
+            car2.MakeHonkNoise();
+            newLot.CarList.Add(car2);
+
+            //Object Initializer Syntax\\
+            Car car3= new Car() { Year = 1998, Make = "Chevrolet", Model = "S-10", EngineNoise = "Wop Wop", HonkNoise = "Aooooooga!", IsDriveable = true};
+                                  
+            car3.MakeEngineNoise();
+            car3.MakeHonkNoise();
+            newLot.CarList.Add(car3);
+            Console.WriteLine();
+
+            Console.WriteLine("__________________________________");
+
+            foreach (var car in newLot.CarList) 
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} and Model: {car.Model}");   
+            }
 
             //*************BONUS*************//
 
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            // Set the properties utilizing the 3 different ways we learned about, one way for each car  *
 
             //*************BONUS X 2*************//
 
